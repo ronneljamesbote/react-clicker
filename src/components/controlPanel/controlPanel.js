d22e8ef59513b9f8abbe90d10256f7e8be562292
ronneljamesdev <ronneljamesdev@pop-os.localdomain>
2019-06-23 14:33:48 +0800

    Add proptypes and defaultprops

--- src/components/controlPanel/controlPanel.js
+++ src/components/controlPanel/controlPanel.js
@@ -1,5 +1,16 @@
+import PropTypes from "prop-types";
+import exact from "prop-types-exact";
 import React from "react";
 import Control from "./control";
+
+controlPanel.propTypes = exact({
+  controls: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string,
+      handler: PropTypes.func
+    }).isRequired
+  ).isRequired
+});
 
 function controlPanel({ controls }) {
   return (
